5bfd456af7e23a09d42c15148e2bcee5ac7fa780
Miraslau <forse2408@gmail.com>
2025-04-16 12:07:35 +0200

    sign-up page with validation has been added

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,20 +1,21 @@
-import React, { Suspense } from 'react';
-import SignInForm from "@/app/ui/form/sign-in-form";
+import React from "react";
 import {lusitana} from "@/app/ui/fonts";
+import SignUpForm from "@/app/ui/form/sign-up-form";
 
-export default function LoginPage() {
+
+export default function SignUpPage() {
     return (
         <div className="space-y-3">
             <div>
                 <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
                     <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-                        Please sign in to continue.
+                        Please sign up to continue.
                     </h1>
                     <div className="w-full">
-                        <SignInForm />
+                        <SignUpForm />
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
--- app/zodSchema/sign-in.schema.ts
+++ app/zodSchema/sign-in.schema.ts
@@ -1,12 +1,24 @@
 import z from "zod";
 
 export const signInSchema = z.object({
-    email: z.string().min(1, {
-        message: "Email address is required"
-    }).email("Invalid email format"),
-    password: z.string().min(1, {
-        message: "Password is required"
-    }),
+    email: z.string().min(1, 'Email address is required'),
+    password: z.string().min(1, 'Password is required'),
 });
+
+export const signUpSchema = z.object({
+    name: z.string().min(1, 'Name is required').max(100),
+    email: z.string().min(1, 'Email address is required').email("Invalid email address"),
+    password: z.string()
+               .min(1, 'Password is required')
+               .min(8, 'Password must have than 8 characters')
+               .refine((password) => /[A-Z]/.test(password), 'Passwords must include uppercase letters')
+               .refine((password) => /[a-z]/.test(password), 'Passwords must include lowercase letters')
+               .refine((password) => /[0-9]/.test(password), 'Passwords must include numbers')
+               .refine((password) => /[!@#$%^&*]/.test(password), 'Passwords must special characters'),
+    confirmPassword: z.string().min(1, 'Password is required')
+}).refine((data) => data.password === data.confirmPassword, {
+    message: 'Confirm passwords must be the same as password',
+    path: ['confirmPassword']
+})
 
 export type User = z.infer<typeof signInSchema>;
